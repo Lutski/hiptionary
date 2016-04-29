ceba3eedb3f431f5c5cb4d0194a83bedeb9bf142
LudvigSoerensen <ludvigsoerensen@gmail.com>
2016-04-29 16:05:32 +0200

    Added 37 words

--- lib/hiptionary/words.rb
+++ lib/hiptionary/words.rb
@@ -7,9 +7,185 @@ module Hiptionary
         example: "Your shoes are amazeballs!"
       },
       {
-        name: "Another word",
-        meaning: "Another meaning",
-        example: "Another example"
+        name: "Totes",
+        meaning: "A shorter way to say totally",
+        example: "I totes need this shirt!"
+      },
+      {
+        name: "Awks",
+        meaning: "A way to describe something as awkward",
+        example: "My conversation with Eric was awks!"
+      },
+      {
+        name: "Devo",
+        meaning: "A term that is short for devastated",
+        example: "I was so devo last night!"
+      },
+      {
+        name: "Grill",
+        meaning: "One's personal business",
+        example: "Bro, you're all up in my grill"
+      },
+      {
+        name: "Povo",
+        meaning: "Short for poverty (being broke)",
+        example: "I really want that coffee but I'm povo"
+      },
+      {
+        name: "Shizz",
+        meaning: "A cooler way to say shit",
+        example: "That shizz was amazing!"
+      },
+      {
+        name: "Shizzle",
+        meaning: "It can mean 'Sure' or 'Shit', depending on the way you say it!",
+        example: "That's the shizzle"
+      },
+      {
+        name: "Chillax",
+        meaning: "A mixture beetween 'chill out' and 'relax'",
+        example: "Chillax dude, you're going to be okay!"
+      },
+      {
+        name: "Whatevs",
+        meaning: "A way to say whatever if you're cool",
+        example: "Whatevs"
+      },
+      {
+        name: "FOMO",
+        meaning: "Fear Of Missing Out",
+        example: "Peter has a huge FOMO"
+      },
+      {
+        name: "Cray Cray",
+        meaning: "When something is really crazy",
+        example: "That party was cray cray"
+      },
+      {
+        name: "Flexitarian",
+        meaning: "An occasional meat eater",
+        example: "I'm a flexitarian, it's like being vegetarian, except I eat meat too."
+      },
+      {
+        name: "Ganic",
+        meaning: "Short for organic, sometimes used to describe something extremely cool",
+        example: "Damn, your pants look ganic bro!"
+      },
+      {
+        name: "Li-fi",
+        meaning: "When you see a free wi-fi connection, join the network but it makes you sign up to some bullshit or tries to charge you.",
+        example: "This place has free wi-fi... Nevermind it's just li-fi!"
+      },
+      {
+        name: "Icicle Fingers",
+        meaning: "When your fingers are so cold, texting is almost impossible",
+        example: "I can't write back because of my icicle fingers!"
+      },
+      {
+        name: "I fuz",
+        meaning: "I'm totally down/I'm cool with that",
+        example: "Yeah, I fuz!"
+      },
+      {
+        name: "lol'd irl",
+        meaning: "When you actually laugh in real life",
+        example: "I lol'd irl when I saw your text!"
+      },
+      {
+        name: "Drake face",
+        meaning: "When you're sadder than sad",
+        example: "I've had drake face all day, because I failed my exam!"
+      },
+      {
+        name: "Luckfully",
+        meaning: "A mixture of thankfully and lucky",
+        example: "Luckfully I got the last item in the store"
+      },
+      {
+        name: "Insta-lerking",
+        meaning: "When you're going more than 25 weeks into someone that you fantasize about's instagram page, without liking any pictures.",
+        example: "I was Insta-lerking Julia last night!"
+      },
+      {
+        name: "Rekt",
+        meaning: "When someone gets completely destroyed",
+        example: "Holy shit brother, you just got REKT!"
+      },
+      {
+        name: "Handufactured",
+        meaning: "When something handcrated, handmade",
+        example: "I just handufactured my own sweater!"
+      },
+      {
+        name: "Scativism",
+        meaning: "Shitting on peoples property as a form of protest.",
+        example: "People used scativism to protest against their neighbours!"
+      },
+      {
+        name: "Name Shame",
+        meaning: "The point at which it's too late to ask for someone's name because you've spent so much time around them that you should already know it.",
+        example: "I have name shame, I've known my co-worker for 2 months and I still don't know his name!"
+      },
+      {
+        name: "DNB",
+        meaning: "A 'Do Noting Bitch', a girl that just want someone to take care of her.",
+        example: "This girl is just a DNB!"
+      },
+      {
+        name: "Selfiecidal",
+        meaning: "A person that takes selfies in dangerous situations or circumstances.",
+        example: "Holy shit, this guy is selfiecidal!"
+      },
+      {
+        name: "Thesaurus Rex",
+        meaning: "A person who uses a synonym word program or internet thesaurus, in order to make their papers appear smarter.",
+        example: "I can't even understand this Thesaurus Rex's paper, there is so many uncommon words."
+      },
+      {
+        name: "Emergenturd",
+        meaning: "A poop that comes so suddenly that one fears they may not be able to make it to the toilet.",
+        example: "I'm so happy I made it to the bathroom, I've got a major emergenturd!"
+      },
+      {
+        name: "Nomophobia",
+        meaning: "Fear of being away from your mobile phone.",
+        example: "I have a serious case of nomophobia!"
+      },
+      {
+        name: "Faceremorse",
+        meaning: "The moment after posting something inflammatory or militant on Facebook that you immediately regret. No matter how quickly it is deleted, people have screenshots.",
+        example: "She deleted her post in a fit or faceremorse, but don't worry I have a screenshot!"
+      },
+      {
+        name: "Narcisexual",
+        meaning: "That you are sexually attracted to yourself.",
+        example: "I think this guy is narcisexual!"
+      },
+      {
+        name: "Lumbersexual",
+        meaning: "A Metro-sexual who has the need to hold on to some outdoor based rugged-ness, thus opting to keep a finely trimmed beard. \n \n OR \n \n
+        A gen y or later man whom is trying desperately to be masculine by looking the part instead of being the part, eg, dressing up like a lumberjack. This is done primarily to capture lost or missing masculinity due to being emasculated by things such as his childhood environment (effeminate father, overbearing soccer mom mother, play dates, trophies for losing.... etc.), feminism, modern urban culture, and 'Sex In The City' reruns. He tries to achieve this masculinity by mimicking real men, and in this particular case a lumberjack due to their connection to blue collar work and an outdoor workplace. To distinguish between the two a number of markers serve as convincing evidence to determine between the real man and the poser. 1. Look at their hands. A lumberjack will have leather for skin, many scars, and sometimes (more often than not) crooked fingers from broken bones not properly set and treated with twigs and duct tape.... a lumbersexual will have smooth, soft hands that smell like lotion. 2. Look at their face. The lumberjack will have weathered skin and probably some scars attributed to flying debris, cable burrs, falling branches, and a number of scenarios involving axes and chain saws. 3. Offer to buy them coffee. The lumberjack will have straight up coffee, maybe with a little cream. The lumbersexual will have some sort of latte or some other fancy schmancy french shit",
+        example: "This guy is lumbersexual!"
+      },
+      {
+        name: "FOGO",
+        meaning: "It means 'Fear Of Going Out', opposite of FOMO (Fear of Missing Out).",
+        example: "I'm have a big FOGO"
+      },
+      {
+        name: "Poptical Illusion",
+        meaning: "Certain patterns on baggy pants that make a girls butt look larger or even in massive tense.",
+        example: "That girls pants are a poptical illusion."
+      },
+      {
+        name: "Bore",
+        meaning: "Something that's boring, and just plain yawn-inducing.",
+        example: "This paper is so bore!"
+      },
+      {
+        name: "Man date",
+        meaning: "Two 'straight' guys doing something that would be your standard date, like going out for a meal or dinner.",
+        example: "Hey Steven, do you want to go on a man date with me. We can go eat dinner!"
       }
     ]
   end
